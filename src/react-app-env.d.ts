2a33607aa02d6a1e4963a541f6d9f8a9d64cb6bc
aazutaku <ymph6cb5jk@outlook.jp>
2023-01-10 00:58:47 +0900

    [add] Headerの追加

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="react-scripts" />
+
+// https://www.newline.co/@dmitryrogozhny/how-to-start-new-react-application-with-create-react-app-and-typescript--4298e606
+// bmp、gif、jpeg、jpg、png、webp、svg、.module.css、.module.scss、.module.sassのインポートに対応
